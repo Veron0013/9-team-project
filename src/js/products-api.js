b36528a5bc1d95fc33d3b9fb29f618a49c07c6a3
Mr.Igor <iv_mirsoft@ukr.net>
2025-07-03 10:13:12 +0300

    api.js

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,7 @@
-// Функції для роботи з бекендом
+import axios from "axios";
+
+export async function getApiData(url) {
+	return await axios.get(url)
+		.then(data => data)
+		.catch(error => error.message);
+};
