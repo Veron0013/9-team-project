d17a093aadeb054beb8ff7c28fbb12bc5a59ea9a
Mr.Igor <iv_mirsoft@ukr.net>
2025-07-03 10:11:40 +0300

    icjnPath + font

--- src/js/refs.js
+++ src/js/refs.js
@@ -6,8 +6,8 @@ export default {
 	END_CATEGORIE_ID: "/books/category",
 	END_BOOK_ID: "/books/",
 	//elements
-	scr_btn_burger_close: "img/ico-sprite.svg#icon-x",
-	scr_btn_burger_normal: "img/ico-sprite.svg#icon-menu-alt-right",
+	scr_btn_burger_close: "/img/ico-sprite.svg#icon-x",
+	scr_btn_burger_normal: "/img/ico-sprite.svg#icon-menu-alt-right",
 	btn_burger: document.querySelector(".burger_menu"),
 	menu_burger: document.querySelector(".modal_menu")
 };
