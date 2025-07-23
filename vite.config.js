9ff2f9e8bd9ac24025614558b07b4b861adb1ff3
Mr.Igor <iv_mirsoft@ukr.net>
2025-07-23 22:04:50 +0300

    path-

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    base: '/9-team-project/',
     root: 'src',
     build: {
       sourcemap: true,
